18f6933700ac89f112d1d5e0256c665a35e55130
Andrew <shevtsov.ap@gmail.com>
2019-12-08 00:37:14 +0100

    Initial Commit

---
+++ SAPSwiftLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAPSwiftLib
+//
+//  Created by ASH on 08.12.2019.
+//  Copyright © 2019 ASH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
